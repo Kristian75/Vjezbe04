a95fd46148808aef6a6fc9c6b63ae19733e81e90
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2024-01-25 12:09:35 +0100

    Add files via upload

---
+++ zadatak13.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz1 = new int[10];
+            int[] niz2 = new int[10];
+            int brojac1 = 0;
+            int brojac2 = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Unesite broj:");
+                int broj = int.Parse(Console.ReadLine());
+
+                if (broj == 0 || brojac1 + brojac2 >= 10)
+                {
+                    break;
+                }
+
+                if (broj % 2 == 0)
+                {
+                    niz2[brojac2] = broj;
+                    brojac2++;
+                }
+                else
+                {
+                    niz1[brojac1] = broj;
+                    brojac1++;
+                }
+            }
+
+            Console.WriteLine("Prvi niz:");
+            for (int i = 0; i < brojac1; i++)
+            {
+                Console.WriteLine(niz1[i]);
+            }
+
+            Console.WriteLine("Drugi niz:");
+            for (int i = 0; i < brojac2; i++)
+            {
+                Console.WriteLine(niz2[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zadatak14.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz1 = { 1, 2, 3, 4, 5 };
+            int[] niz2 = { 1, 2, 5, 4, 5 };
+
+            int brojClanova = 0;
+
+            for (int i = 0; i < niz1.Length; i++)
+            {
+                if (niz1[i] == niz2[i])
+                {
+                    brojClanova++;
+                }
+            }
+
+            Console.WriteLine("Broj članova s istim indeksom i vrijednošću: " + brojClanova);
+
+            Console.ReadKey();
+        }
+    }
+}
